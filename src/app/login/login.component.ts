4c93e461109e27b628a2f1599b83dd569bf36b40
Manohar4333 <thappetlamanohar@gmail.com>
2024-11-04 07:03:07 +0530

    Login functionality added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  password:String=""
+  username:String=""
+  reqpwd:String=""
+  constructor(private bs:Router) {}
+  
+  checkLogin(){ 
+    this.reqpwd= this.username.slice(0,3)+"123";
+    if(this.password == this.reqpwd){
+      this.bs.navigate(['user/home']);
+    }
+    else{
+      alert("Invalid Password");
+    }
+  }
+
 
 }
